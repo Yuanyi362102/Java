07db3e45f2ba8a201523eebf3d2c587ff6ba2716
yuanyi <yuanyi691@qq.com>
2024-03-21 19:30:15 +0800

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	static int a=1;
+public static void main(String[] args) {
+System.out.println("怎么样？");
+	for(int i=0;i<5;i++){
+		a=a*2;
+	}	
+	System.out.println(a);
+}
+
+}
